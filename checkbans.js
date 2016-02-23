e513cc1bcd4db54084dce7f161479c1aa70f2367
ge-ku <geku94@gmail.com>
2016-02-23 19:52:24 +0200

    Option to remove green text about no bans

--- checkbans.js
+++ checkbans.js
@@ -83,7 +83,7 @@ javascript:(function(){
 
                 span.style.color = 'rgb(255, 73, 73)';
                 span.innerHTML = text;
-            } else {
+            } else if (greentext){
                 span.style.color = 'rgb(43, 203, 64)';
                 span.innerHTML = 'No Bans for this player.';
             }
@@ -113,7 +113,16 @@ javascript:(function(){
 	var defaultkeys = ["5DA40A4A4699DEE30C1C9A7BCE84C914",
 						"5970533AA2A0651E9105E706D0F8EDDC",
 						"2B3382EBA9E8C1B58054BD5C5EE1C36A"];
-	chrome.storage.sync.get("customapikey", function(data) {
+	var greentext = true;
+	chrome.storage.sync.get(["customapikey", "greentext"], function(data) {
+		if (typeof data['greentext'] == 'undefined'){
+			chrome.storage.sync.set({
+				'greentext': true
+			});
+		} else {
+			greentext = data['greentext'];
+		}
+		console.log(greentext);
 		if (typeof data['customapikey'] == 'undefined'){
 			var apikey = defaultkeys[Math.floor(Math.random() * 3)];
 		}else{
@@ -123,6 +132,6 @@ javascript:(function(){
 		while (ids.length > 0) {
 			var batch = ids.splice(0, 100);
 			makeApiCall(batch, apikey);
-		}
-	});  
+		} 
+	});
 })();
--- setkey.js
+++ setkey.js
@@ -2,41 +2,46 @@ function save_options() {
   var customapikey = document.getElementById('customapikey').value;
   if(document.getElementById('radioCustom').checked && customapikey != '') {
   //use custom key
-	chrome.storage.sync.set({
-		customapikey: customapikey
-	}, function() {
-    // update status
-		var status = document.getElementById('status');
-		status.textContent = 'Options saved.';
-		setTimeout(function() {
-			status.textContent = '';
-		}, 750);
+	chrome.storage.sync.set({"customapikey": customapikey}, function() {
+		chrome.storage.sync.set({"greentext": !(document.getElementById('chkGreentext').checked)}, function() {
+			var status = document.getElementById('status');
+			status.textContent = 'Options saved.';
+			setTimeout(function() {
+				status.innerHTML = '<br>';
+			}, 750);
+		});
 	});
   }else if(document.getElementById('radioDefault').checked || customapikey == '') {
   //use default key
 	chrome.storage.sync.remove('customapikey', function() {
-		var status = document.getElementById('status');
-		status.textContent = 'Options saved.';
-		document.getElementById('radioDefault').checked = true;
-		document.getElementById('customapikey').value = '';
-		setTimeout(function() {
-			status.textContent = '';
-		}, 750);
+		chrome.storage.sync.set({"greentext": !(document.getElementById('chkGreentext').checked)}, function() {
+			var status = document.getElementById('status');
+			status.textContent = 'Options saved.';
+			document.getElementById('radioDefault').checked = true;
+			document.getElementById('customapikey').value = '';
+			setTimeout(function() {
+				status.innerHTML = '<br>';
+			}, 750);
+		});
 	});
   }
 }
 
 function restore_options() {
-	chrome.storage.sync.get("customapikey", function(data) {
+	chrome.storage.sync.get(["customapikey", "greentext"], function(data){
 		if (typeof data['customapikey'] == 'undefined'){
 			
 		}else{
 			document.getElementById('customapikey').value = data['customapikey'];
 			document.getElementById('radioCustom').checked = true;
 		}
-	});
+		if (typeof data['greentext'] == 'undefined'){
+			
+		} else if (data['greentext'] == false){
+			document.getElementById('chkGreentext').checked = true;
+		}
+	})
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+document.getElementById('save').addEventListener('click', save_options);
